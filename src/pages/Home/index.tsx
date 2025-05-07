754478c21887b969d87835d77f4e6eb38739eb7d
davidsnts <david.santos@delage.com.br>
2025-05-06 23:25:19 -0300

    Consumindo API

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,122 +1,43 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+
+interface productProps {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  cover: string;
+}
+
 export function Home() {
+  const [products, setProducts] = useState<productProps[]>([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const response = await api.get("/products");
+      setProducts(response.data);
+    }
+    getProducts();
+  }, []);
+  console.log(products);
   return (
     <main className="grid max-w-7xl grid-cols-1 h-full md:grid-cols-2 lg:grid-cols-4 mx-auto place-items-center gap-y-10 gap-x-16 p-4">
-      <section className="w-[300px] flex flex-col justify-center items-center border-gray-300  border-2 rounded-2xl p-5">
-        <img
-          src="https://images.tcdn.com.br/img/img_prod/1087789/racao_super_dog_caes_adultos_carne_e_cereais_25kg_1185_1_f0d10acd64d03c2f1883a238278d52bf.jpg"
-          className="w-[300px] h-[300px]"
-          alt=""
-        />
-        <div className="w-full">Ração para cães adultos</div>
-        <div className="flex w-full flex-col gap-4 mt-3">
-          <span className="font-bold">R$ 42,00</span>
-          <span className="text-center bg-green-950 rounded-md text-white font-bold p-2">Adicionar ao carrinho</span>
-        </div>
-      </section>
-
-      <section className="w-[300px] flex flex-col justify-center items-center border-gray-300  border-2 rounded-2xl p-5">
-        <img
-          src="https://images.tcdn.com.br/img/img_prod/1087789/racao_super_dog_caes_adultos_carne_e_cereais_25kg_1185_1_f0d10acd64d03c2f1883a238278d52bf.jpg"
-          className="w-[300px] h-[300px]"
-          alt=""
-        />
-        <div className="w-full">Ração para cães adultos</div>
-        <div className="flex w-full flex-col gap-4 mt-3">
-          <span className="font-bold">R$ 42,00</span>
-          <span className="text-center bg-green-950 rounded-md text-white font-bold p-2">Adicionar ao carrinho</span>
-        </div>
-      </section>
-
-      <section className="w-[300px] flex flex-col justify-center items-center border-gray-300  border-2 rounded-2xl p-5">
-        <img
-          src="https://images.tcdn.com.br/img/img_prod/1087789/racao_super_dog_caes_adultos_carne_e_cereais_25kg_1185_1_f0d10acd64d03c2f1883a238278d52bf.jpg"
-          className="w-[300px] h-[300px]"
-          alt=""
-        />
-        <div className="w-full">Ração para cães adultos</div>
-        <div className="flex w-full flex-col gap-4 mt-3">
-          <span className="font-bold">R$ 42,00</span>
-          <span className="text-center bg-green-950 rounded-md text-white font-bold p-2">Adicionar ao carrinho</span>
-        </div>
-      </section>
-
-      <section className="w-[300px] flex flex-col justify-center items-center border-gray-300  border-2 rounded-2xl p-5">
-        <img
-          src="https://images.tcdn.com.br/img/img_prod/1087789/racao_super_dog_caes_adultos_carne_e_cereais_25kg_1185_1_f0d10acd64d03c2f1883a238278d52bf.jpg"
-          className="w-[300px] h-[300px]"
-          alt=""
-        />
-        <div className="w-full">Ração para cães adultos</div>
-        <div className="flex w-full flex-col gap-4 mt-3">
-          <span className="font-bold">R$ 42,00</span>
-          <span className="text-center bg-green-950 rounded-md text-white font-bold p-2">Adicionar ao carrinho</span>
-        </div>
-      </section>
-
-      <section className="w-[300px] flex flex-col justify-center items-center border-gray-300  border-2 rounded-2xl p-5">
-        <img
-          src="https://images.tcdn.com.br/img/img_prod/1087789/racao_super_dog_caes_adultos_carne_e_cereais_25kg_1185_1_f0d10acd64d03c2f1883a238278d52bf.jpg"
-          className="w-[300px] h-[300px]"
-          alt=""
-        />
-        <div className="w-full">Ração para cães adultos</div>
-        <div className="flex w-full flex-col gap-4 mt-3">
-          <span className="font-bold">R$ 42,00</span>
-          <span className="text-center bg-green-950 rounded-md text-white font-bold p-2">Adicionar ao carrinho</span>
-        </div>
-      </section>
-
-      <section className="w-[300px] flex flex-col justify-center items-center border-gray-300  border-2 rounded-2xl p-5">
-        <img
-          src="https://images.tcdn.com.br/img/img_prod/1087789/racao_super_dog_caes_adultos_carne_e_cereais_25kg_1185_1_f0d10acd64d03c2f1883a238278d52bf.jpg"
-          className="w-[300px] h-[300px]"
-          alt=""
-        />
-        <div className="w-full">Ração para cães adultos</div>
-        <div className="flex w-full flex-col gap-4 mt-3">
-          <span className="font-bold">R$ 42,00</span>
-          <span className="text-center bg-green-950 rounded-md text-white font-bold p-2">Adicionar ao carrinho</span>
-        </div>
-      </section>
-
-      <section className="w-[300px] flex flex-col justify-center items-center border-gray-300  border-2 rounded-2xl p-5">
-        <img
-          src="https://images.tcdn.com.br/img/img_prod/1087789/racao_super_dog_caes_adultos_carne_e_cereais_25kg_1185_1_f0d10acd64d03c2f1883a238278d52bf.jpg"
-          className="w-[300px] h-[300px]"
-          alt=""
-        />
-        <div className="w-full">Ração para cães adultos</div>
-        <div className="flex w-full flex-col gap-4 mt-3">
-          <span className="font-bold">R$ 42,00</span>
-          <span className="text-center bg-green-950 rounded-md text-white font-bold p-2">Adicionar ao carrinho</span>
-        </div>
-      </section>
-
-      <section className="w-[300px] flex flex-col justify-center items-center border-gray-300  border-2 rounded-2xl p-5">
-        <img
-          src="https://images.tcdn.com.br/img/img_prod/1087789/racao_super_dog_caes_adultos_carne_e_cereais_25kg_1185_1_f0d10acd64d03c2f1883a238278d52bf.jpg"
-          className="w-[300px] h-[300px]"
-          alt=""
-        />
-        <div className="w-full">Ração para cães adultos</div>
-        <div className="flex w-full flex-col gap-4 mt-3">
-          <span className="font-bold">R$ 42,00</span>
-          <span className="text-center bg-green-950 rounded-md text-white font-bold p-2">Adicionar ao carrinho</span>
-        </div>
-      </section>
-
-      <section className="w-[300px] flex flex-col justify-center items-center border-gray-300  border-2 rounded-2xl p-5">
-        <img
-          src="https://images.tcdn.com.br/img/img_prod/1087789/racao_super_dog_caes_adultos_carne_e_cereais_25kg_1185_1_f0d10acd64d03c2f1883a238278d52bf.jpg"
-          className="w-[300px] h-[300px]"
-          alt=""
-        />
-        <div className="w-full">Ração para cães adultos</div>
-        <div className="flex w-full flex-col gap-4 mt-3">
-          <span className="font-bold">R$ 42,00</span>
-          <span className="text-center bg-green-950 rounded-md text-white font-bold p-2">Adicionar ao carrinho</span>
-        </div>
-      </section>
+      {products.map((item) => (
+        <section key={item.id} className="w-[300px] flex flex-col justify-center items-center border-gray-300  border-2 rounded-2xl p-5">
+          <img
+            src={item.cover}
+            className="w-[300px] h-[300px]"
+            alt=""
+          />
+          <div className="w-full">{item.title}</div>
+          <div className="flex w-full flex-col gap-4 mt-3">
+            <span className="font-bold">{item.price}</span>
+            <span className="text-center bg-green-950 rounded-md text-white font-bold p-2">
+              Adicionar ao carrinho
+            </span>
+          </div>
+        </section>
+      ))}
     </main>
   );
 }
---
+++ src/services/api.ts
@@ -0,0 +1,6 @@
+import  axios  from "axios";
+
+export const api = axios.create({
+    baseURL: "http://localhost:3000"
+})
+// json-server --watch db.json
